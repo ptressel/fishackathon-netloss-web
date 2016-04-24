47dd8f5feffcf9c9c33d7ebda16843413c4e90ef
Pat Tressel <ptressel@myuw.net>
2016-04-23 21:32:00 -0700

    Add a map element. Attempt to add markers. They're not showing up.

--- app/reports/reports.js
+++ app/reports/reports.js
@@ -5,9 +5,42 @@
 
   app.controller('ReportsCtrl', ['$scope', 'reportsList', function($scope, reportsList) {
       $scope.reports = reportsList;
+
+      var mapOptions = {
+          zoom: 12,
+          center: new google.maps.LatLng(47.5234, -122.0097),
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+      }
+      $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+      //$scope.markers = [];
+      var infoWindow = new google.maps.InfoWindow();
+      var createMarker = function (report){
+          var marker = new google.maps.Marker({
+              map: $scope.map,
+              position: new google.maps.LatLng(report.Latitude, report.Longitude),
+              title: report.name
+          });
+          marker.content = '<div class="infoWindowContent">' + report.$id + '</div>';
+          google.maps.event.addListener(report, 'click', function(){
+              infoWindow.setContent('<h2>' + report.$id + '</h2>' + marker.content);
+              infoWindow.open($scope.map, marker);
+          });
+          //$scope.markers.push(marker);
+      }  
+
+      var i;
+      for (i = 0; i < reportsList.length; i++){
+          createMarker(reportsList[i]);
+      }
+
+      $scope.openInfoWindow = function(e, selectedMarker){
+          e.preventDefault();
+          google.maps.event.trigger(selectedMarker, 'click');
+      }
     }]);
 
-    app.factory('reportsList', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
+  app.factory('reportsList', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
     var ref = fbutil.ref('Reports');
     return $firebaseArray(ref);
   }]);
