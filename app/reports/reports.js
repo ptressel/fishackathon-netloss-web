11290fffa5fe69ccfe3feca1148de561bcb50f5c
Ryan Bruels <bruels@spacestation6.com>
2016-04-24 01:42:24 -0700

    Remove old factory and wait for the $loaded event instead. Also add a function to add map markers when the data reloads

--- app/reports/reports.js
+++ app/reports/reports.js
@@ -3,47 +3,80 @@
 
   var app = angular.module('netloss.reports', ['ngRoute', 'firebase.utils', 'firebase']);
 
-  app.controller('ReportsCtrl', ['$scope', 'reportsList', function($scope, reportsList) {
-      $scope.reports = reportsList;
+  app.controller('ReportsCtrl', ['$scope', 'fbutil', '$firebaseArray', function($scope, fbutil, $firebaseArray) {
+      
+      // Load the Reports ref directly in here
+      var $netlossArray = $firebaseArray.$extend({    
+          // This is called anytime a new object is added   
+          $$added: function(dataSnapshot){
+              var user = dataSnapshot.val(); //This is the data
+              
+              return user; 
+          },
+          $$updated: function(dataSnapshot){
+             //An update has been made
+             var changed = false; 
+             //Go through your data, if changed change the variable. 
+             return changed;
+          }
+      });
+    
+      var ref = fbutil.ref('Reports');
+      var reportsList = $firebaseArray(ref);
+      // $loaded gets called whenever all items are actually loaded
+      reportsList.$loaded(function() {
+        
+        // Expose "reports" to the template
+        $scope.reports = reportsList;
+      
+        var mapOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(47.5234, -122.0097),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-      var mapOptions = {
-          zoom: 12,
-          center: new google.maps.LatLng(47.5234, -122.0097),
-          mapTypeId: google.maps.MapTypeId.TERRAIN
-      }
-      $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        //$scope.markers = [];
+        var infoWindow = new google.maps.InfoWindow();
+        var createMarker = function (report){
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(report.Latitude, report.Longitude),
+                title: report.name
+            });
+            marker.content = '<div class="infoWindowContent">' + report.$id + '</div>';
+            google.maps.event.addListener(report, 'click', function(){
+                infoWindow.setContent('<h2>' + report.$id + '</h2>' + marker.content);
+                infoWindow.open($scope.map, marker);
+            });
+            //$scope.markers.push(marker);
+        }  
 
-      //$scope.markers = [];
-      var infoWindow = new google.maps.InfoWindow();
-      var createMarker = function (report){
-          var marker = new google.maps.Marker({
-              map: $scope.map,
-              position: new google.maps.LatLng(report.Latitude, report.Longitude),
-              title: report.name
-          });
-          marker.content = '<div class="infoWindowContent">' + report.$id + '</div>';
-          google.maps.event.addListener(report, 'click', function(){
-              infoWindow.setContent('<h2>' + report.$id + '</h2>' + marker.content);
-              infoWindow.open($scope.map, marker);
-          });
-          //$scope.markers.push(marker);
-      }  
-
-      var i;
-      for (i = 0; i < reportsList.length; i++){
-          createMarker(reportsList[i]);
-      }
-
-      $scope.openInfoWindow = function(e, selectedMarker){
-          e.preventDefault();
-          google.maps.event.trigger(selectedMarker, 'click');
-      }
+        // Function to create map markers
+        var createMapMarkers = function() {
+          var i;      
+          for (i = 0; i < reportsList.length; i++){
+              createMarker(reportsList[i]);
+          }
+        }
+      
+        // Call it for initial load
+        createMapMarkers();
+        
+        // $watch is called when the data model changes, for us to 
+        // reload the map pins
+        $scope.reports.$watch(function (data) {
+          // TODO we may need to wipe the old map markers?
+          createMapMarkers();
+        });
+        
+        $scope.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+        }
+      });
+      
     }]);
-
-  app.factory('reportsList', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
-    var ref = fbutil.ref('Reports');
-    return $firebaseArray(ref);
-  }]);
 
   app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.whenAuthenticated('/reports', {
